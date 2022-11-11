ba3fe3adcaa67dbe2453ec1fcb7d843ebdd1036a
kieutuananh2112 <0306201208@caothang.edu.vn>
2022-11-11 16:56:41 +0700

    them lai mua vat pham

---
+++ DA_Moble/lib/Screen/Muavatpham.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MuavatphamScreen extends StatefulWidget {
+  const MuavatphamScreen({super.key});
+
+  @override
+  State<MuavatphamScreen> createState() => _MuavatphamScreenState();
+}
+
+class _MuavatphamScreenState extends State<MuavatphamScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text('Hồ Sơ'),
+                Icon(Icons.settings),
+              ],
+            ),
+            Row(
+              children: [
+                Image(
+                    height: 50,
+                    width: 50,
+                    image: AssetImage(
+                      'assets/images/language.png',
+                    )),
+                Text('Chỉnh sửa hồ sơ')
+              ],
+            ),
+            Text('Thống kê'),
+            Container(
+              child: Row(
+                children: [
+                  Column(
+                    children: [
+                      OutlinedButton(
+                          onPressed: () {}, child: Text('Tổng điểm KN')),
+                      OutlinedButton(
+                          onPressed: () {}, child: Text('Trận đã chơi')),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      OutlinedButton(
+                          onPressed: () {}, child: Text('Tổng điểm KN')),
+                      OutlinedButton(
+                          onPressed: () {}, child: Text('Trận đã chơi')),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Text('Thống kê'),
+            Row(
+              children: [
+                Column(
+                  children: [
+                    OutlinedButton(onPressed: () {}, child: Text('Thành tích')),
+                    OutlinedButton(onPressed: () {}, child: Text('Thành tích')),
+                    OutlinedButton(onPressed: () {}, child: Text('Thành tích')),
+                    OutlinedButton(onPressed: () {}, child: Text('Thành tích')),
+                    OutlinedButton(onPressed: () {}, child: Text('Thành tích')),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

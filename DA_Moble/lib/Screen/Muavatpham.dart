8cf21052f9654650c78d438c3d2a454d404081f2
kieutuananh2112 <0306201208@caothang.edu.vn>
2022-11-11 16:49:39 +0700

    sua font

--- DA_Moble/lib/Screen/Muavatpham.dart
+++ DA_Moble/lib/Screen/Muavatpham.dart
@@ -13,52 +13,65 @@ class _MuavatphamScreenState extends State<MuavatphamScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body: Container(child: Column(children: [
-          Row(
-            children: [
-              Text('Ho so'),
-              Icon(Icons.settings),
-            ],
-          ),
-          Row(children: [
-            Image(
-              height: 50,
-              width: 50,
-              image: AssetImage('assets/images/language.png',)),
-            Text('Chinh sua ho so')
-          ],),
-          Text('Thống kê'),
-          Container(
-            
-            child: 
-            Row(children: [
-            Column(children: [
-                OutlinedButton(onPressed: (){}, child: Text('Tổng điểm KN')),
-                OutlinedButton(onPressed: (){}, child: Text('Trận đã chơi')),
-            ],),
-            Column(children: [
-                OutlinedButton(onPressed: (){}, child: Text('Tổng điểm KN')),
-                OutlinedButton(onPressed: (){}, child: Text('Trận đã chơi')),
-            ],),
-            
-          ],),
-          
-          ),
-           Text('Thống kê'),
-           Row(children: [
-            Column(children: [
-                  OutlinedButton(onPressed: (){}, child: Text('Thành tích') ),
-                  OutlinedButton(onPressed: (){}, child: Text('Thành tích') ),
-                  OutlinedButton(onPressed: (){}, child: Text('Thành tích') ),
-                  OutlinedButton(onPressed: (){}, child: Text('Thành tích') ),
-                  OutlinedButton(onPressed: (){}, child: Text('Thành tích') ),
-            ],)
-              
-           ],)
-      ],),
-      
-    
+      body: Container(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text('Hồ Sơ'),
+                Icon(Icons.settings),
+              ],
+            ),
+            Row(
+              children: [
+                Image(
+                    height: 50,
+                    width: 50,
+                    image: AssetImage(
+                      'assets/images/language.png',
+                    )),
+                Text('Chỉnh sửa hồ sơ')
+              ],
+            ),
+            Text('Thống kê'),
+            Container(
+              child: Row(
+                children: [
+                  Column(
+                    children: [
+                      OutlinedButton(
+                          onPressed: () {}, child: Text('Tổng điểm KN')),
+                      OutlinedButton(
+                          onPressed: () {}, child: Text('Trận đã chơi')),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      OutlinedButton(
+                          onPressed: () {}, child: Text('Tổng điểm KN')),
+                      OutlinedButton(
+                          onPressed: () {}, child: Text('Trận đã chơi')),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Text('Thống kê'),
+            Row(
+              children: [
+                Column(
+                  children: [
+                    OutlinedButton(onPressed: () {}, child: Text('Thành tích')),
+                    OutlinedButton(onPressed: () {}, child: Text('Thành tích')),
+                    OutlinedButton(onPressed: () {}, child: Text('Thành tích')),
+                    OutlinedButton(onPressed: () {}, child: Text('Thành tích')),
+                    OutlinedButton(onPressed: () {}, child: Text('Thành tích')),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }

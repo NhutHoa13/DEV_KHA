b64c75b52f5e62f56e3685eb7f738c695c7acc4a
teo <0306201250@caothang.edu.vn>
2022-11-17 15:21:51 +0700

    Choi cá nhân

---
+++ DA_Moble/lib/Screen/Choicanhan.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ChoicanhanScreen extends StatefulWidget {
+  const ChoicanhanScreen({super.key});
+
+  @override
+  State<ChoicanhanScreen> createState() => _ChoicanhanScreenState();
+}
+
+class _ChoicanhanScreenState extends State<ChoicanhanScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

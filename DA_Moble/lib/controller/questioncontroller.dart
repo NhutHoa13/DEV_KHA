4b2d3b0686192d37b10402b49c94406fc1f9351d
NhutHoa13 <0306201240@caothang.edu.vn>
2022-12-12 23:13:15 +0700

    nhuthoa

--- DA_Moble/lib/controller/questioncontroller.dart
+++ DA_Moble/lib/controller/questioncontroller.dart
@@ -1,22 +1,24 @@
 
-// import 'package:flutter/animation.dart';
-// import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:flutter/animation.dart';
+import 'package:get/get.dart';
 
-// class QuestionController extends GetxController with SingleGetTickerProviderMixin {
 
-// late AnimationController _animationController;
-// late Animation _animation;
-// Animation get animation => this._animation;
-// @override
-// void onInit(){
-//   _animationController = AnimationController(duration: Duration(seconds: 60),vsync: this);
-//   _animation = Tween<double>(begin: 0, end: 1).animate(_animationController)
-//   ..addListener(() {
-//     update();  
-//   });
+class QuestionController extends GetxController {}
 
-//   _animationController.forward();
-//   super.onInit();
-// }
+late AnimationController _animationController;
+late Animation _animation;
+Animation get animation => this._animation;
+@override
+void onInit(){
+  _animationController = AnimationController(duration: Duration(seconds: 60),vsync: this);
+  _animation = Tween<double>(begin: 0, end: 1).animate(_animationController)
+  ..addListener(() {
+    update();  
+  });
 
-// }
+  _animationController.forward();
+  super.onInit();
+}
+
+}
+

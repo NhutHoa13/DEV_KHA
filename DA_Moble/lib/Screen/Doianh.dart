a98f66eac907a3262242f2b4733c984b9057fb5a
kieutuananh2112 <0306201208@caothang.edu.vn>
2022-12-21 14:55:00 +0700

    them chuc nang avatar đang loi import

--- DA_Moble/lib/Screen/Doianh.dart
+++ DA_Moble/lib/Screen/Doianh.dart
@@ -2,53 +2,63 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_application_1/Screen/Chinhsuahoso.dart';
+import 'package:flutter_application_1/Screen/chucnang/page_update_avatar.dart';
 
 class DoianhScreen extends StatefulWidget {
   const DoianhScreen({super.key});
 
   @override
   State<DoianhScreen> createState() => _DoianhScreenState();
+
+  static void getProfile(BuildContext context) {}
+
+  static void nextPage(
+      BuildContext context, PageUpdateAvatar pageUpdateAvatar) {}
 }
 
 class _DoianhScreenState extends State<DoianhScreen> {
-    var orange = Color.fromARGB(255, 255, 172, 47);
+  var orange = Color.fromARGB(255, 255, 172, 47);
   @override
-
   Widget build(BuildContext context) {
     var mlr = 20.0;
     var r = MediaQuery.of(context).size.width - mlr;
     var l = MediaQuery.of(context).size.width - mlr;
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: SingleChildScrollView(child: Container(
-         margin: EdgeInsets.fromLTRB(0, mlr+10, 0, 0),
-         child: Column(children: [
-          Row(
-             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.fromLTRB(0, mlr + 10, 0, 0),
+          child: Column(
             children: [
-            InkWell(
-                            onTap: () {
-                              Navigator.of(context).popUntil((route) => route.isFirst);
-                              Navigator.push(context, MaterialPageRoute(builder: (context) => const ChinhsuahosoScreen()));
-                            },
-                            child: Icon(
-                              Icons.arrow_back,
-                              color: Colors.black.withOpacity(0.6),
-                              size: 30,
-                            ),
-                          ),
-                          
-                        
-                        Text(
-                          'Avatar',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black.withOpacity(0.6),
-                              fontSize: 16),
-                        ),
-                        Text('      ')
-          ],),
-           Padding(
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.of(context).popUntil((route) => route.isFirst);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  const ChinhsuahosoScreen()));
+                    },
+                    child: Icon(
+                      Icons.arrow_back,
+                      color: Colors.black.withOpacity(0.6),
+                      size: 30,
+                    ),
+                  ),
+                  Text(
+                    'Avatar',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black.withOpacity(0.6),
+                        fontSize: 16),
+                  ),
+                  Text('      ')
+                ],
+              ),
+              Padding(
                 padding: const EdgeInsets.fromLTRB(0, 20, 0, 30),
                 child: Stack(
                   children: [
@@ -78,60 +88,75 @@ class _DoianhScreenState extends State<DoianhScreen> {
                   ],
                 ),
               ),
-               Text(
-                  'Cập nhật Avatar',
-                  style: TextStyle(
-                      fontSize: 18, fontWeight: FontWeight.bold, color: orange),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                 for(var i=1; i < 5; i++ )...{
-                  Column(children: [
-                     for(var i=1; i < 5; i++ )...{
-                         Container(
-                    padding: EdgeInsets.all(20),
-                      alignment: Alignment.center,
-                      child: Expanded(
-                        child: SizedBox(
-                          width: 50,
-                          height: 50,
-                          child: ClipOval(
-                            child: Image(
-                              image: AssetImage('assets/images/profile.png'),
-                              fit: BoxFit.cover,
+              Text(
+                'Cập nhật Avatar',
+                style: TextStyle(
+                    fontSize: 18, fontWeight: FontWeight.bold, color: orange),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  for (var i = 1; i < 5; i++) ...{
+                    Column(
+                      children: [
+                        for (var i = 1; i < 5; i++) ...{
+                          Container(
+                            padding: EdgeInsets.all(20),
+                            alignment: Alignment.center,
+                            child: Expanded(
+                              child: SizedBox(
+                                width: 50,
+                                height: 50,
+                                child: ClipOval(
+                                  child: Image(
+                                    image:
+                                        AssetImage('assets/images/profile.png'),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                      ),
-                    ),
-                     }
-                  ],)
-                 
-                
-              },],),
-            InkWell(
-          onTap: () {
-            Navigator.pop(context);
-          },
-          child: Container(
-            margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
-            width: 250,
-            height: 50,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10), color: orange),
-                child: Column(mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                 Text(
-                'Quay lại',
-                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold,color: Colors.white),
+                        },
+                        TextButton.icon(
+                            onPressed: () {
+                              DoianhScreen.nextPage(
+                                  context, PageUpdateAvatar());
+                            },
+                            icon: const Icon(Icons.camera_alt_rounded),
+                            label: Text('change')),
+                      ],
+                    )
+                  },
+                ],
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Container(
+                    margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                    width: 250,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10), color: orange),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Quay lại',
+                          style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        )
+                      ],
+                    )),
               )
-                ],)
+            ],
           ),
-        )
-         ],),
-      )
-      ,),
+        ),
+      ),
     );
   }
 }
